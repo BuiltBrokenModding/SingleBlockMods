17ba2a4215f55c1f5988f16c2f681dddafc937c0
Robert Seifert <rseifert.phone@gmail.com>
2015-07-25 11:33:26 -0400

    Added textures and lang file for wooden cart

--- woodenRails/src/main/java/com/builtbroken/woodenrails/cart/ItemWoodenCart.java
+++ woodenRails/src/main/java/com/builtbroken/woodenrails/cart/ItemWoodenCart.java
@@ -1,13 +1,16 @@
 package com.builtbroken.woodenrails.cart;
 
+import com.builtbroken.woodenrails.WoodenRails;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockDispenser;
 import net.minecraft.block.BlockRailBase;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -17,9 +20,13 @@ import java.util.List;
  */
 public class ItemWoodenCart extends Item
 {
+    @SideOnly(Side.CLIENT)
+    public static IIcon furnaceMinecraft;
+
     public ItemWoodenCart()
     {
         this.maxStackSize = 3;
+        this.setUnlocalizedName(WoodenRails.PREFIX + "WoodenCart");
         this.setCreativeTab(CreativeTabs.tabTransport);
         BlockDispenser.dispenseBehaviorRegistry.putObject(this, new DispenserMinecartBehavior());
     }
@@ -47,6 +54,21 @@ public class ItemWoodenCart extends Item
             }
         }
         return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public IIcon getIconFromDamage(int meta)
+    {
+        return this.itemIcon;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IIconRegister reg)
+    {
+        this.itemIcon = reg.registerIcon(WoodenRails.PREFIX + "minecart_normal");
+        this.furnaceMinecraft = reg.registerIcon(WoodenRails.PREFIX + "minecart_furnace");
     }
 
     @SideOnly(Side.CLIENT)
