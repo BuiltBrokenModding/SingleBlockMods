f1494acb9dc0eb44712e0a58cefa775c8917472b
dmodoomsirius <dmodoomsirius@gmail.com>
2016-04-06 09:50:48 -0400

    remove all SBM mods as they are thir own repos now

--- coloredPots/src/main/java/com/builtbroken/coloredpots/ClientProxy.java
+++
@@ -1,9 +0,0 @@
-package com.builtbroken.coloredpots;
-
-/**
- * Created by Dark on 7/25/2015.
- */
-public class ClientProxy extends CommonProxy
-{
-
-}
--- coloredPots/src/main/java/com/builtbroken/coloredpots/pot/BlockPot.java
+++
@@ -1,8 +0,0 @@
-package com.builtbroken.coloredpots.pot;
-
-/**
- * Created by Dark on 7/28/2015.
- */
-public class BlockPot extends net.minecraft.block.BlockFlowerPot
-{
-}
